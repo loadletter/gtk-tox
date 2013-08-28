284a96e6c6ecb0a77ebc4db14c32c531b2eb3074
loadletter <loadletter@users.noreply.github.com>
2013-08-28 19:04:01 +0200

    Moved resolve_addr and hex_string_to_bin to misc.c

---
+++ src/misc.c
@@ -0,0 +1,60 @@
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+
+/*
+  resolve_addr():
+    address should represent IPv4 or a hostname with A record
+
+    returns a data in network byte order that can be used to set IP.i or IP_Port.ip.i
+    returns 0 on failure
+
+    TODO: Fix ipv6 support
+*/
+uint32_t resolve_addr(const char *address)
+{
+    struct addrinfo *server = NULL;
+    struct addrinfo  hints;
+    int              rc;
+    uint32_t         addr;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family   = AF_INET;    // IPv4 only right now.
+    hints.ai_socktype = SOCK_DGRAM; // type of socket Tox uses.
+
+    rc = getaddrinfo(address, "echo", &hints, &server);
+
+    // Lookup failed.
+    if (rc != 0) {
+        return 0;
+    }
+
+    // IPv4 records only..
+    if (server->ai_family != AF_INET) {
+        freeaddrinfo(server);
+        return 0;
+    }
+
+
+    addr = ((struct sockaddr_in *)server->ai_addr)->sin_addr.s_addr;
+
+    freeaddrinfo(server);
+    return addr;
+}
+
+// XXX: FIX
+unsigned char *hex_string_to_bin(char hex_string[])
+{
+    size_t len = strlen(hex_string);
+    unsigned char *val = malloc(len);
+    char *pos = hex_string;
+    int i;
+
+    for (i = 0; i < len; ++i, pos += 2)
+        sscanf(pos, "%2hhx", &val[i]);
+
+    return val;
+}
---
+++ src/misc.h
@@ -0,0 +1,3 @@
+
+uint32_t resolve_addr(const char *address);
+unsigned char *hex_string_to_bin(char hex_string[]);
