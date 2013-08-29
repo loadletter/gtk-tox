b83b18d600658c4ee37befe6b995430ea720f305
loadletter <loadletter@users.noreply.github.com>
2013-08-29 22:20:22 +0200

    Working DHT tab, TODO: cleanup

--- src/dht.c
+++ src/dht.c
@@ -19,8 +19,13 @@ static void printip(ipbuf buf, IP ip)
 
 void dht_draw(struct dht_tree_data *dht_d)
 {
-    Messenger *m = dht_d->m;
-    GtkWidget *dht_treeview = dht_d->gtk;
+    Messenger   *m = dht_d->m;
+    GtkWidget   *dht_treeview = dht_d->gtk;
+    GtkListStore *store;
+    GtkTreeIter iter;
+
+    store = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(dht_treeview)));
+    gtk_list_store_clear(store);
     
     Client_data *close_clientlist = DHT_get_close_list(m->dht);
     
@@ -32,23 +37,39 @@ void dht_draw(struct dht_tree_data *dht_d)
     for (i = 0; i < LCLIENT_LIST; i++) { /*Number of nodes in closelist*/
     
         Client_data *client = close_clientlist + i;
-
         uint16_t port = ntohs(client->ip_port.port);
-
+        char tmp_id[(CLIENT_ID_SIZE * 2) + 1], tmp_port[6];
+        
         if (port) {
+            gtk_list_store_append(store, &iter);
+            memset(tmp_id, '\0', sizeof(tmp_id));
+            memset(tmp_port, '\0', sizeof(tmp_port));
+            
             for (j = 0; j < CLIENT_ID_SIZE; j++)
-                printf("%02hhx", client->client_id[j]);
+                sprintf(tmp_id,"%s%02hhx", tmp_id, client->client_id[j]);
+            
+            printf("%s", tmp_id);
+            gtk_list_store_set(store, &iter, 0, tmp_id, -1);
 
             printip(ipbuf, client->ip_port.ip);
             printf("  %15s %5u ", ipbuf, port);
             printf("  %3llu ", now - client->timestamp);
             printf("  %3llu ", now - client->last_pinged);
-
+            gtk_list_store_set(store, &iter, 1, ipbuf, -1);
+            sprintf(tmp_port, "%5u", port);            
+            gtk_list_store_set(store, &iter, 2, tmp_port, -1);
+            gtk_list_store_set(store, &iter, 3, now - client->timestamp, -1);
+            gtk_list_store_set(store, &iter, 4, now - client->last_pinged, -1);
+            
             port = ntohs(client->ret_ip_port.port);
 
             if (port) {
                 printip(ipbuf, client->ret_ip_port.ip);
                 printf("  %15s %5u  %3llu", ipbuf, port, now - close_clientlist[i].ret_timestamp);
+                gtk_list_store_set(store, &iter, 5, ipbuf, -1);
+                sprintf(tmp_port, "%5u", port);
+                gtk_list_store_set(store, &iter, 6, tmp_port, -1);
+                gtk_list_store_set(store, &iter, 7, now - close_clientlist[i].ret_timestamp, -1);
             }
         }
 
