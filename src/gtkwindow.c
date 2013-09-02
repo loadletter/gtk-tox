99557da41a995e9487403c4819cfc459d02f4476
loadletter <loadletter@users.noreply.github.com>
2013-09-02 19:44:41 +0200

    Added function to make a friendrequest dialog. Fixed identation of gtkwindow.c.

--- src/gtkwindow.c
+++ src/gtkwindow.c
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include "gtkwindow.h"
 
 /* treeview */
 /* TODO: make it useful */
@@ -30,36 +31,78 @@ void tw_clear_list(GtkWidget *list)
 
 void note_hide_page(GtkNotebook *notebook, int pagenumber)
 {
-        GtkWidget *page;
+    GtkWidget *page;
 
-        page = gtk_notebook_get_nth_page(notebook, pagenumber);
-        gtk_widget_hide(page);
+    page = gtk_notebook_get_nth_page(notebook, pagenumber);
+    gtk_widget_hide(page);
 }
 
 void note_show_page(GtkNotebook *notebook, int pagenumber)
 {
-        GtkWidget *page;
+    GtkWidget *page;
 
-        page = gtk_notebook_get_nth_page(notebook, pagenumber);
-        gtk_widget_show(page);
-        gtk_notebook_set_current_page(notebook, pagenumber);
+    page = gtk_notebook_get_nth_page(notebook, pagenumber);
+    gtk_widget_show(page);
+    gtk_notebook_set_current_page(notebook, pagenumber);
 }
 
-void error_message (const gchar *message)
+void dialog_show_error(const gchar *message)
 {
-        GtkWidget               *dialog;
-        
-        /* log to terminal window */
-        g_warning (message);
-        
-        /* create an error message dialog and display modally to the user */
-        dialog = gtk_message_dialog_new (NULL, 
-                                         GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
-                                         GTK_MESSAGE_ERROR,
-                                         GTK_BUTTONS_OK,
-                                         message);
-        
-        gtk_window_set_title (GTK_WINDOW (dialog), "Error!");
-        gtk_dialog_run (GTK_DIALOG (dialog));      
-        gtk_widget_destroy (dialog);         
+    GtkWidget               *dialog;
+    
+    /* log to terminal window */
+    g_warning (message);
+    
+    /* create an error message dialog and display modally to the user */
+    dialog = gtk_message_dialog_new (NULL, 
+                                     GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+                                     GTK_MESSAGE_ERROR,
+                                     GTK_BUTTONS_OK,
+                                     message);
+    
+    gtk_window_set_title (GTK_WINDOW (dialog), "Error!");
+    gtk_dialog_run (GTK_DIALOG (dialog));      
+    gtk_widget_destroy (dialog);         
+}
+
+gint dialog_show_friendrequest(gpointer window, gchar *text_id, gchar *text_msg)
+{
+    GtkWidget *dialog;
+    GtkWidget *box;
+    GtkWidget *label;
+    GtkWidget *image;
+    gchar     *text;
+    gint rv;
+    
+    dialog = gtk_dialog_new_with_buttons("New friend request!",
+                                          GTK_WINDOW(window),
+                                          GTK_DIALOG_DESTROY_WITH_PARENT,
+                                          "_Accept", GTK_RESPONSE_ACCEPT,
+                                          "_Ignore", GTK_RESPONSE_CANCEL,
+                                          NULL );
+    
+    box = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
+    
+    text = g_strdup_printf ("You received a new friend request with the message:\n\"%s\"\nFrom the following ID:\n%s", text_msg, text_id);
+    label = gtk_label_new(text);
+    g_free(text);
+    
+    image = gtk_image_new_from_file(ICON_FRIEND_ADD);
+    
+    gtk_box_pack_start(GTK_BOX(box), image, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(box), label, TRUE, TRUE, 0);
+    
+    gtk_widget_show_all(dialog);
+
+    rv = gtk_dialog_run(GTK_DIALOG(dialog));
+    
+    if(rv == GTK_RESPONSE_ACCEPT) {
+    g_print("yes\n");
+    } else {
+    g_print("no\n");
+    }
+    
+    gtk_widget_destroy(dialog);
+    
+    return rv;
 }
--- src/gtkwindow.h
+++ src/gtkwindow.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <tox/tox.h>
 
 struct gtox_data {
     Tox     *tox;
@@ -22,6 +23,7 @@ enum
   NOTEBOOK_DHT = 2
 };
 
+#define ICON_FRIEND_ADD "icons/user_add.png"
 
 void tw_add_to_list(GtkWidget *list, int list_item, const gchar *str);
 void tw_clear_list(GtkWidget *list);
@@ -29,4 +31,6 @@ void tw_clear_list(GtkWidget *list);
 void note_hide_page(GtkNotebook *notebook, int pagenumber);
 void note_show_page(GtkNotebook *notebook, int pagenumber);
 
-void error_message (const gchar *message);
+void dialog_show_error(const gchar *message);
+
+gint dialog_show_friendrequest(gpointer window, gchar *text_id, gchar *text_msg);
